9c15a97ceb89dcfb271f14861ec4abcb8918c3e4
Mazharul Islam <mazharulislam.emon@outlook.com>
2019-06-21 15:14:40 +0600

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', function(e) {
     pone.textContent = 'Loading...'
     ptwo.textContent = '';
     const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
 .then( res => {
     res.json().then(data => {
         if(data.error){
