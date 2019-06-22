143334be4866b5be8b9f6489ea0a27758e1f1435
Mazharul Islam <mazharulislam.emon@outlook.com>
2019-06-22 21:46:21 +0600

    adding features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 function forecast(latitude,longitude,callback){
 	const url = 'https://api.darksky.net/forecast/11ddc71691c9cb12702a6b1fb8f94cda/' + latitude
-	 +  ',' +  longitude;
+	 +  ',' +  longitude+ '?units=ca';
    request({url: url, json: true}, (err,{body} = {}) => {
    	if(err){
    		callback('unable to connect to the api . please check your network',undefined);
     } else if(body.code) {
     	callback('Unable to find location. Try another search.', undefined);
     } else {
-    	callback(undefined, `the summary of the place is ${body.currently.summary} and icon is ${body.currently.icon}. temperature is ${body.currently.temperature}`);
+		callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degress out. This high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}.There is a ${body.currently.precipProbability}% chance of rain.`);
     }	
    });
 }
